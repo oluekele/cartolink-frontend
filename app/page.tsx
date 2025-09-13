bf45410ebf52dc930de6d3cfdbf3c12619cc624e
unknown <ekeleolu1@gmail.com>
2025-09-13 14:10:41 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1 p-6 max-w-5xl mx-auto w-full">
+      <main className="flex-1 p-6 max-w-7xl mx-auto w-full">
         <h2 className="text-2xl font-bold mb-6">Projects</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
           <Card title="Project Alpha" subtitle="Frontend • React" />
           <Card title="Project Beta" subtitle="Design • Figma" />
           <Card title="Project Gamma" subtitle="Mobile • React Native" />
--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ import { useThemeStore } from "@/lib/store/theme-store"
 export default function Header() {
   const { dark } = useThemeStore()
   return (
-    <header className="flex items-center justify-between p-6 border-b dark:border-slate-700">
+    <header className="flex items-center justify-between p-6 lg:px-14  border-b dark:border-slate-700">
       <div className="flex items-center gap-3">
         <Image src="/Colorful-PNG.webp" width={50} height={50} alt="logo" className="w-10 h-10 rounded-full" />
         <div>
